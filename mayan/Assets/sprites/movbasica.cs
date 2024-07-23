805d32c9df51489e4c4f202f73c1da7278860fe6
uzumaki007 <123951417+uzumaki10987654321@users.noreply.github.com>
2024-07-23 15:37:14 -0300

    greg

--- mayan/Assets/sprites/movbasica.cs
+++ mayan/Assets/sprites/movbasica.cs
@@ -12,7 +12,17 @@ public class movbasica : MonoBehaviour
     public Transform posicaoSensor;
     public bool sensor;
 
-    private
+
+    //criar uma variável para verificar a direção
+    public bool verificarDirecaoPersonagem;
+
+    //Configurando tiro
+    public GameObject municao;
+    public Transform posicaoTiro;
+    public float velocidadeTiro;
+
+
+    private Animator anim;
     void Start()
     {
         rbPlayer = GetComponent<Rigidbody2D>();
@@ -28,12 +38,37 @@ public class movbasica : MonoBehaviour
         if (Input.GetButtonDown("Jump") && sensor==true)
         {
             rbPlayer.AddForce(new Vector2(0, forcaPulo));
-        } 
+        }
+
+        if (Input.GetButtonDown(0))
+        {
+            Atirar();
+        }
+
+        anim.SetInteger("Run", (int)movimentoHorizontal);
+        anim.SetBool("sensor",sensor);
     }
 
 
     public void verificarChao()
     {
         sensor = Physics2D.OverlapCircle(posicaoSensor.position, 0.34f); 
+    }
+
+    public void Flip()
+    {
+        verificarDirecaoPersonagem = !verificarDirecaoPersonagem;
+
+        float x = transform.localScale.x * -1;
+
+        transform.localScale = new Vector3(x, transform.localScale.y,transform.localScale.z);
+    }
+
+
+    public void Atirar()
+    {
+        GameObject temporario = Instantiate(municao);
+        temporario.transform.position = posicaoTiro.position;
+        temporario.GetComponent<Rigidbody2D>().velocity = new Vector2(velocidadeTiro, 0);
     }
 }
